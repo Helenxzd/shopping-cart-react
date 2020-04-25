e9b966bca854b5ebccf80231491d3e32687954e1
duxinzhe <naomixzdu@gmail.com>
2020-04-25 11:06:34 -0500

    user can signup and login with Google accounts

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React, { useEffect, useState } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Typography from "@material-ui/core/Typography";
 
 import ProductCardList from "./components/ProductCardList";
 
@@ -20,12 +24,54 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database().ref();
 
+const uiConfig = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ],
+  callbacks: {
+    signInSuccessWithAuthResult: () => false
+  }
+};
+
+const SignIn = () => (
+    <StyledFirebaseAuth
+        uiConfig={uiConfig}
+        firebaseAuth={firebase.auth()}
+    />
+);
+
+const Welcome = ({ user }) => {
+  return (
+      <div style={{ width: '100%' }}>
+        <Box bgcolor="text.primary" color="background.paper" display="flex">
+          <Box p={1} flexGrow={1} alignItems="center">
+            <Typography>Welcome, {user.displayName}</Typography>
+          </Box>
+          <Box p={1}>
+            <Button variant="contained" onClick={() => firebase.auth().signOut()}>
+              Log out
+            </Button>
+          </Box>
+        </Box>
+      </div>
+  );
+};
+
+const Banner = ({ user }) => (
+    <React.Fragment>
+      { user ? <Welcome user={ user } /> : <SignIn /> }
+    </React.Fragment>
+);
+
 const App = () => {
   const [data, setData] = useState({});
   const products = Object.values(data);
 
   const [inv, setInv] = useState({});
   const inventory = inv;
+
+  const [user, setUser] = useState(null);
 
 
    // useEffect(() => {
@@ -60,6 +106,10 @@ const App = () => {
     fetchProduct();
   }, []);
 
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(setUser);
+  }, []);
+
   // useEffect(() => {
   //     const handleData = snap => {
   //       setInv(snap.val());
@@ -74,6 +124,7 @@ const App = () => {
   return (
       // <ProductCardList products={products}/>
       <React.Fragment>
+        <Banner user={ user } />
         <ProductCardList products={products} inventory={inventory}/>
       </React.Fragment>
       // <React.Fragment>
--- src/components/ProductCardList.js
+++ src/components/ProductCardList.js
@@ -3,15 +3,12 @@ import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import FloatCart from "./FloatCart";
 
-
 import ProductCard from "./ProductCard"
 import Selector from "./Selector";
-
 
 export default function ProductCardList({products, inventory}) {
     const [cartOpen, setCartOpen] = useState(false);
     const [cartList, setCartList] = useState([]);
-
 
     return (
         <React.Fragment>
